c86c8011fd80f067822e8e3c7999cb05fc581193
vruiz0107 <vickieruiz@ymail.com>
2023-07-15 18:24:02 -0400

    added to spots.js

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,2 +1,40 @@
 const express = require('express');
 const router = express.Router();
+const {check, query } = require('express-validator');
+const {handleValidationErrors} = require('../../utils/validation');
+
+const validateSpot = [
+    check('address')
+        .exists({ checkFalsy: true })
+        .withMessage('Street address is required'),
+
+    check('city')
+        .exists({ checkFalsy: true }) 
+        .withMessage('Street address is required'),
+
+    check('state')
+        .exists({ checkFalsy: true })
+        .withMessage('State is required'),
+
+    check('country')
+        .exists({ checkFalsy: true })
+        .withMessage('Country is required'),
+
+    check('lat')
+        .exists({ checkFalsy: true })
+        .withMessage('Latitude is not valud').bail()
+        .isNumeric({ checkFalsy: true })
+        .withMessage('Latitude is not valid'),
+
+    check('lng')
+        .exists({ checkFalsy: true })
+        .withMessage('Longitude is not valid').bail()
+        .isNumeric({ checkFalsy: true })
+        .withMessage('Longitude is not valid'),
+
+    // not done:
+    // check('name')
+    //     .exists({ checkFalsy: true })
+    //     .withMessage('Name must be less than 50 characters'),
+    
+]
